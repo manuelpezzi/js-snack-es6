ec10a310bd7c4aff12f80cbc7c99203dbb615322
manuelpezzi <pezzimanuel@gmail.com>
2025-01-22 13:00:33 +0100

    inizio snack 2

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
-
+const squadreCalcio = [
+    { nome: "Juventus", punti: 0, falliSubiti: 0 },
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
+    { nome: "Inter", punti: 0, falliSubiti: 0 },
+    { nome: "Napoli", punti: 0, falliSubiti: 0 },
+    { nome: "Roma", punti: 0, falliSubiti: 0 }
+];
 
 
 /*Snack 3 (Bonus)
